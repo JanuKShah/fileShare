bd2aee5b924f4fd5ba5888235101d02ff66746bf
JanuKShah <janukshah4@gmail.com>
2021-08-07 00:59:20 +0530

    cors setup

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+require('dotenv').config();
 const express=require('express');
 const app=express();
 const path = require('path');
-
+const cors = require('cors');
 const PORT=process.env.PORT || 3000;
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+  }
+
+app.use(cors(corsOptions));
 app.use(express.static('public'));
 app.use(express.json());
+//DB
 const connectDB=require('./config/db');
-
 connectDB();
 //Template Engine
 app.set('views', path.join(__dirname, '/views'));
